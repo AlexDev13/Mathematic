4e08a97477f70f95dcb8d20eb0ebb9922d209a58
Alexander <https://github.com/AlexDev13>
2019-08-16 15:35:12 +0600

    6kyu complete

--- code/FindUniq.go
+++ code/FindUniq.go
@@ -1,11 +1,16 @@
 package code
 
-import (
-	"fmt"
-)
+import ()
 
-func FindUniq(arr []float32){
-	
-		
-
+func FindUniq(arr []float32) float32 {
+	keys := make(map[float32]int)
+	for i := 0; i < len(arr); i++ {
+		keys[arr[i]]++
+	}
+	for key, value := range keys {
+		if value == 1 {
+			arr[0] = key
+		}
+	}
+	return arr[0]
 }
