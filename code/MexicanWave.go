11dfd88e16e9efd4d295f11485117dd9c446be7f
Alexander <https://github.com/AlexDev13>
2019-08-16 10:29:24 +0600

    next

--- code/MexicanWave.go
+++ code/MexicanWave.go
@@ -7,8 +7,9 @@ import (
 
 func Wave(words string) {
 	for i:=1;i<=len(words);i++{
-		fmt.Println(strings.ToUpper(words))
-		// fmt.Println(i,words)
+		for i:=0;i<len(words);i++{
+			fmt.Println(strings.ToUpper(words[i]))
+		}		
 	}
 
 }
